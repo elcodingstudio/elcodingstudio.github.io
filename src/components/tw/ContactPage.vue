567a548f71bf4d450056c782ded17722eeb90226
Edden Tsai <eddentsai@gmail.com>
2023-06-01 00:06:41 +0800

    新增LINE加好友的按鈕

--- src/components/tw/ContactPage.vue
+++ src/components/tw/ContactPage.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="container pt-3">
       <h1 class="text-center mt-4">聯絡我們</h1>
-      <div class="row">
-        <div class="col-md mt-1">
-          <h5>加入官方Line id:@356vawoa</h5>
-          <h5>Email: elcodingstudio@gmail.com</h5>
+      <div class="row justify-content-center align-items-center mt-3">
+        <div class="col-md">
+          <div>
+            <div class="h5">加入官方Line id:@356vawoa</div>
+            <a href="https://lin.ee/vqfTBsa" target="_blank"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="36"></a>
+          </div>
         </div>
+      </div>
+      <div class="row justify-content-center align-items-center mt-4">
+        <div class="col-md"><h5>Email: elcodingstudio@gmail.com</h5></div>
       </div>
     </div>
   </template>
